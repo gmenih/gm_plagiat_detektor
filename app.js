93c5ad03a7e44455cc848428b85a6830a7de8bf4
Gregor Menih <gmenih@gmail.com>
2015-01-07 16:11:54 +0100

    Added file dialog open & file count

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
 var app = angular.module('gm', ['ngRoute', 'ngAnimate']);
 app.config(['$routeProvider', function($routeProvider) {
-	$routeProvider.
-	when('/', {
-		templateUrl: 'partials/fileSelect.html',
-		controller: 'FileSelectController'
-	}).
-	when('/results', {
-		templateUrl: 'partials/results.html'
-	})
+  $routeProvider.
+  when('/', {
+    templateUrl: 'partials/fileSelect.html',
+    controller: 'FileSelectController'
+  }).
+  when('/results', {
+    templateUrl: 'partials/results.html'
+  })
 }])
 
 app.controller('FileSelectController', function($scope) {
-	var dialogOpen = false;
-	// checkboxes
-	$scope.stringMatchCB = true;
-	$scope.backOfWordsCB = false;
-	$scope.showContentCB = true;
+  var dialogOpen = false;
+  // checkboxes
+  $scope.stringMatchCB = true;
+  $scope.backOfWordsCB = false;
+  $scope.showContentCB = true;
+  // spisek datoteks
+  $scope.listOfFiles = [];
 
-	$scope.OpenFileDialog = function() {
-		var fileDialog = angular.element('#hiddenFileDialog');
-		if (!dialogOpen) {
-			fileDialog.trigger('click');
-			dialogOpen = true;
-		}
-		fileDialog.on('change', function(e) {
-			console.log('files added');
-
-		})
-	}
-})
+  var fileDialog = angular.element('#hiddenFileDialog');
+  // odpri dialog za izbiro datotek
+  $scope.OpenFileDialog = function() {
+    console.log('ofd clicked');
+    if (!dialogOpen) {
+      fileDialog.trigger('click');
+      fileDialog.on('change', function() {
+        console.log('this');
+        var fileList = fileDialog[0].files;
+        for (var i = 0; i < fileList.length; i++) {
+          $scope.$apply(function() {
+            $scope.listOfFiles.push(new MyFile(fileList[i]));
+          })
+        }
+      });
+      dialogOpen = true;
+    }
+  }
+});

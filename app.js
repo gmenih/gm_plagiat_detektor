a597e088c78808ffc323d295e50e20dd941d0108
Gregor Menih <gmenih@gmail.com>
2015-01-07 17:55:31 +0100

    detektor class, body disable

--- app.js
+++ app.js
@@ -1,40 +1,52 @@
 var app = angular.module('gm', ['ngRoute', 'ngAnimate']);
 app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-  when('/', {
-    templateUrl: 'partials/fileSelect.html',
-    controller: 'FileSelectController'
-  }).
-  when('/results', {
-    templateUrl: 'partials/results.html'
-  })
+	$routeProvider.
+	when('/', {
+		templateUrl: 'partials/fileSelect.html',
+		controller: 'FileSelectController'
+	}).
+	when('/results', {
+		templateUrl: 'partials/results.html'
+	})
 }])
 
 app.controller('FileSelectController', function($scope) {
-  var dialogOpen = false;
-  // checkboxes
-  $scope.stringMatchCB = true;
-  $scope.backOfWordsCB = false;
-  $scope.showContentCB = true;
-  // spisek datoteks
-  $scope.listOfFiles = [];
+	$scope.dialogOpen = false;
+	// checkboxes
+	$scope.stringMatchCB = true;
+	$scope.clearResultsCB = true;
+	$scope.showContentCB = true;
+	// spisek datoteks
+	$scope.listOfFiles = [];
 
-  var fileDialog = angular.element('#hiddenFileDialog');
-  // odpri dialog za izbiro datotek
-  $scope.OpenFileDialog = function() {
-    console.log('ofd clicked');
-    if (!dialogOpen) {
-      fileDialog.trigger('click');
-      fileDialog.on('change', function() {
-        console.log('this');
-        var fileList = fileDialog[0].files;
-        for (var i = 0; i < fileList.length; i++) {
-          $scope.$apply(function() {
-            $scope.listOfFiles.push(new MyFile(fileList[i]));
-          })
-        }
-      });
-      dialogOpen = true;
-    }
-  }
+	var fileDialog = angular.element('#hiddenFileDialog');
+	// odpri dialog za izbiro datotek
+	$scope.OpenFileDialog = function() {
+		if (!$scope.dialogOpen) {
+			// hack to check if user cancels the dialog
+			fileDialog[0].files.append(new File("", ""));
+			// trigger click on dialog
+			fileDialog.trigger('click');
+			$scope.dialogOpen = true;
+			// track selection changes
+			fileDialog.on('change', function() {
+				if (fileDialog.val() != '') {
+					var fileList = fileDialog[0].files;
+					for (var i = 0; i < fileList.length; i++) {
+						$scope.$apply(function() {
+							$scope.listOfFiles.push(new MyFile(fileList[i]));
+						})
+					}
+				}
+				fileDialog.val('');
+				$scope.$apply(function() {
+					$scope.dialogOpen = false;
+				})
+			});
+		}
+	}
+
+	$scope.CheckFiles = function() {
+		Detektor.StringMatch($scope.listOfFiles[0], $scope.listOfFiles[1]);
+	}
 });
